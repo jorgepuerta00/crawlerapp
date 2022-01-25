b9b83a01abf9a182ff4bfd8a91a4d12818df8755
Jorge Puerta <jorge.puerta00@gmail.com>
2022-01-24 21:41:40 -0500

    first code

---
+++ CrawlerApp/Crawler.cs
@@ -0,0 +1,127 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrawlerApp
+{
+    public class Crawler
+    {
+        private readonly ISet<string> _urlListCache;
+        private readonly string _mainDomainUrl;
+        private readonly Uri _mainDomainUri;
+        private readonly string _mode;
+        private readonly string _startCommand;
+
+        public Crawler(string[] args)
+        {
+            this._startCommand = args[0];
+            this._mode = args[1];
+            this._mainDomainUrl = args[2].NormalizeUrl();
+            this._mainDomainUri = new Uri(this._mainDomainUrl);
+            this._urlListCache = new HashSet<string>();
+        }
+
+        public async Task Execute()
+        {
+            if (!this._startCommand.Equals("wcraw"))
+            {
+                Console.WriteLine("The command to start the program is not valid!");
+                return;
+            }
+            if (!this._mainDomainUrl.IsValidUrl())
+            {
+                Console.WriteLine("The url is not valid!");
+                return;
+            }
+            if (!(this._mode.Equals("-r") || this._mode.Equals("-n")))
+            {
+                Console.WriteLine("The entered mode is not valid");
+                return;
+            }
+
+            await this.ExecuteHttpRequest(this._mainDomainUrl);
+        }
+
+        private async Task ExecuteHttpRequest(string url)
+        {
+            var client = new RestClient();
+            var request = new RestRequest(url)
+            {
+                Timeout = 6000,
+                Method = Method.Get
+            };
+            var cancellationTokenSource = new CancellationTokenSource();
+            var response = await client.ExecuteAsync(request, cancellationTokenSource.Token);
+            this.PrintResponse(response);
+            await this.ProcessResponse(response);
+        }
+
+        private void PrintResponse(RestResponse response)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Resolving... " + (response.Request.Resource));
+            Console.WriteLine("Response... " + ((int)response.StatusCode).ToString() + " " + (response.StatusDescription));
+            Console.WriteLine();
+        }
+
+        private async Task ProcessResponse(RestResponse response)
+        {
+            var urlList = this.GetNewUrlListFromHtmlContent(response.Content);
+
+            foreach (var url in urlList)
+            {
+                if (this._urlListCache.Contains(url) is false)
+                {
+                    this._urlListCache.Add(url.ToString());
+
+                    if (this._mode.Equals("-r"))
+                        await this.ExecuteHttpRequest(url);
+                }
+            }
+        }
+
+        private ISet<string> GetNewUrlListFromHtmlContent(string content)
+        {
+            if (string.IsNullOrEmpty(content))
+                return new HashSet<string>();
+
+            ISet<string> newUrlList = new HashSet<string>();
+            MatchCollection matches = GetUrlMatchesFromHtmlContent(content);
+
+            foreach (var match in matches)
+            {
+                var matchUri = new Uri(match.ToString());
+
+                if (matchUri.IsInsideTheMainDomain(this._mainDomainUri))
+                    this.AddUrlSegmentsToList(newUrlList, matchUri);
+            }
+            return newUrlList;
+        }
+
+        private static MatchCollection GetUrlMatchesFromHtmlContent(string content)
+        {
+            Regex regexLink = new Regex(@"(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])");
+            var matches = regexLink.Matches(content);
+            return matches;
+        }
+
+        private void AddUrlSegmentsToList(ISet<string> urlList, Uri matchUri)
+        {
+            var segmentUrl = new StringBuilder();
+            segmentUrl.Append(matchUri.Scheme + "://")
+                      .Append(matchUri.Host);
+
+            foreach (var segment in matchUri.Segments)
+            {
+                segmentUrl.Append(segment);
+                var normalizedUrl = segmentUrl.ToString().NormalizeUrl();
+                if (urlList.Contains(normalizedUrl) is false)
+                    urlList.Add(normalizedUrl);
+            }
+        }
+    }
+}
---
+++ CrawlerApp/UrlNormalization.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace CrawlerApp
+{
+    public static class UrlNormalization
+    {
+        public static bool AreTheSameUrls(this string url1, string url2)
+        {
+            url1 = url1.NormalizeUrl();
+            url2 = url2.NormalizeUrl();
+            return url1.Equals(url2);
+        }
+
+        public static bool AreTheSameUrls(this Uri uri1, Uri uri2)
+        {
+            var url1 = uri1.NormalizeUrl();
+            var url2 = uri2.NormalizeUrl();
+            return url1.Equals(url2);
+        }
+
+        private static string[] DefaultDirectoryIndexes = new[]
+            {
+                "default.asp",
+                "default.aspx",
+                "index.htm",
+                "index.html",
+                "index.php"
+            };
+
+        public static string NormalizeUrl(this Uri uri)
+        {
+            var url = UrlToLower(uri);
+            url = LimitProtocols(url);
+            url = RemoveDefaultDirectoryIndexes(url);
+            url = RemoveTheFragment(url);
+            url = RemoveDuplicateSlashes(url);
+            url = AddWww(url);
+            url = RemoveFeedburnerPart(url);
+            return RemoveTrailingSlashAndEmptyQuery(url);
+        }
+
+        public static string NormalizeUrl(this string url)
+        {
+            return NormalizeUrl(new Uri(url));
+        }
+
+        public static bool IsInsideTheMainDomain(this Uri currentUri, Uri domainUri)
+        {
+            string sDomainUrl = domainUri.Host + domainUri.AbsolutePath;
+            string currentUrl = currentUri.Host + currentUri.Segments.FirstOrDefault();
+            return sDomainUrl.Contains(currentUrl);
+        }
+
+        public static bool IsValidUrl(this string url)
+        {
+            Regex regexLink = new Regex(@"(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])");
+            return regexLink.IsMatch(url);
+        }
+
+        private static string RemoveFeedburnerPart(string url)
+        {
+            var idx = url.IndexOf("utm_source=", StringComparison.Ordinal);
+            return idx == -1 ? url : url.Substring(0, idx - 1);
+        }
+
+        private static string AddWww(string url)
+        {
+            if (new Uri(url).Host.Split('.').Length == 2 && !url.Contains("://www."))
+            {
+                return url.Replace("://", "://www.");
+            }
+            return url;
+        }
+
+        private static string RemoveDuplicateSlashes(string url)
+        {
+            var path = new Uri(url).AbsolutePath;
+            return path.Contains("//") ? url.Replace(path, path.Replace("//", "/")) : url;
+        }
+
+        private static string LimitProtocols(string url)
+        {
+            return new Uri(url).Scheme == "https" ? url.Replace("https://", "http://") : url;
+        }
+
+        private static string RemoveTheFragment(string url)
+        {
+            var fragment = new Uri(url).Fragment;
+            return string.IsNullOrWhiteSpace(fragment) ? url : url.Replace(fragment, string.Empty);
+        }
+
+        private static string UrlToLower(Uri uri)
+        {
+            return HttpUtility.UrlDecode(uri.AbsoluteUri.ToLowerInvariant());
+        }
+
+        private static string RemoveTrailingSlashAndEmptyQuery(string url)
+        {
+            return url
+                    .TrimEnd(new[] { '?' })
+                    .TrimEnd(new[] { '/' });
+        }
+
+        private static string RemoveDefaultDirectoryIndexes(string url)
+        {
+            foreach (var index in DefaultDirectoryIndexes)
+            {
+                if (url.EndsWith(index))
+                {
+                    url = url.TrimEnd(index.ToCharArray());
+                    break;
+                }
+            }
+            return url;
+        }
+    }
+}
